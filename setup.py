b8b0379167cbafcaa76dacfd84553c0b61f9879c
nathants <me@nathants.com>
2018-03-04 20:37:41 -0800

    modernize setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setuptools.setup(
     name="py-schema",
     author='nathan todd-stone',
     author_email='me@nathants.com',
+    python_requires='>=3.6',
     url='http://github.com/nathants/py-schema',
+    install_requires=['py-util'],
+    dependency_links=['https://github.com/nathants/py-util/tarball/4d1fe20ecfc0b6982933a8c9b622b1b86da2be5e#egg=py-util-0.0.1'],
     packages=['schema'],
     description='data centric schema validation',
 )
