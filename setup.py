e9584f4959318d904b171c9e208bd9fe49c0d5ad
nathants <me@nathants.com>
2018-12-19 13:12:16 -0800

    pin to avoid pypi aliasing

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author_email='me@nathants.com',
     python_requires='>=3.7',
     url='http://github.com/nathants/py-schema',
-    install_requires=['py-util'],
+    install_requires=['py-util==0.0.1'],
     dependency_links=['https://github.com/nathants/py-util/tarball/fa60dbf761a61beb94614af89240fd5986d26786#egg=py-util-0.0.1'],
     packages=['schema'],
     description='data centric schema validation',
