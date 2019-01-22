ff6b9cea02991d5dca6abf23808123aa562e02c4
nathants <me@nathants.com>
2019-01-22 15:12:49 -0800

    dependency links are no more

--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
 ### data centric schema validation
 
 ### installation
-`pip install --process-dependency-links git+https://github.com/nathants/py-schema@d76cf70 # or some other git hash`
+```
+git clone https://github.com/nathants/py-schema
+cd py-schema
+pip install -r requirements.txt .
+```
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
 
-
 setuptools.setup(
     version="0.0.1",
     license='mit',
@@ -9,8 +8,6 @@ setuptools.setup(
     author_email='me@nathants.com',
     python_requires='>=3.7',
     url='http://github.com/nathants/py-schema',
-    install_requires=['py-util==0.0.1'],
-    dependency_links=['https://github.com/nathants/py-util/tarball/fa60dbf761a61beb94614af89240fd5986d26786#egg=py-util-0.0.1'],
     packages=['schema'],
     description='data centric schema validation',
 )
