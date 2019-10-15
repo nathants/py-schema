f284f278edd8aecb6b4c495e68fbbdb85c225637
nathants <me@nathants.com>
2019-10-15 12:02:32 -0700

    test against pypy3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     name="py-schema",
     author='nathan todd-stone',
     author_email='me@nathants.com',
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     url='http://github.com/nathants/py-schema',
     packages=['schema'],
     description='data centric schema validation',
